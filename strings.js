16d9e9423bc6333b963e03d5b35d44de870694c5
Chris Evans <realchrisevans@outlook.com>
2022-04-18 19:58:31 -0400

    update

--- strings.js
+++ strings.js
@@ -29,4 +29,55 @@ for (let i = 0; i < string.length; i++) {
     }
 } console.log("The number of vowels is " + vowels);
 
+// Write a program to count the number of words in a string.
+
+let string = 'Hello World!';
+let words = 1;
+
+for (let i = 0; i < string.length; i++) {
+    if (string.charAt(i) === ' ') {
+        words++;
+    }
+} 
+console.log("The number of words is " + words);
+
+// Write a program to get all the character in a string uniquely.
+
+let string = 'Hello World!';
+let unique = '';
+
+for (let i = 0; i < string.length; i++) {
+    if (unique.indexOf(string.charAt(i)) === -1) {
+        unique += string.charAt(i);
+    }
+}
+console.log(unique);
+
+// Write a program to check if a string is present another string.
+
+let string = 'Hello World!';
+let string2 = 'World';
+
+while (string.length > 0) {
+    if (string.indexOf(string2) !== -1) {
+        console.log("The string " + string2 + " is present in the string " + string);
+    }
+    string = string.substring(1);
+} console.log("The string " + string2 + " is present in the string " + string);
+
+
+// Write a program that capitalizes the first letter of all words in a sentence.
+
+let string = 'hello world!';
+let string2 = '';
+
+while (string.length > 0) {
+    if (string.charAt(0) === ' ') {
+        string2 += string.charAt(0);
+        string = string.substring(1);
+    } else {
+        string2 += string.charAt(0).toUpperCase();
+        string = string.substring(1);
+    }
+} console.log(string2);
 
